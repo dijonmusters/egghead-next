4e9e4b582c770ce4a3ebbb12c7da916221b1dcbb
Daniel Miller <dealingwith@gmail.com>
2020-10-07 13:07:47 -0500

    Adds a single Cypress test
    updates the README

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,4 +3,11 @@ describe('egghead is working', () => {
     cy.visit('/')
     cy.contains('©egghead.io')
   })
+  it('homepage contains nav', () => {
+    cy.visit('/')
+    cy.get('nav > ul > li').should(($li) => {
+      // currently [Topics] [Search]
+      expect($li).length.to.be.at.least(2)
+    })
+  })
 })
